0a2edc5125bb4acdcf89d5cfcaf7f3bb38f07b3e
Rich Ricot <richricot@gmail.com>
2024-06-02 16:20:46 +0000

    Inside a view

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world! MTFBWY")
+    if request.method == "POST"
+        return HttpResponse("You must have POSTed something")
+    elif 
+        return HttpResponse(request.method)
