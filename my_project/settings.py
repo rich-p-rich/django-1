65cf0f54a2f35e438588dd54fc56417f73a1beb5
Rich Ricot <richricot@gmail.com>
2024-06-05 20:13:48 +0000

    Add pip3 freeze to requirements.txt

--- my_project/settings.py
+++ my_project/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['8000-richprich-django1-hp8fwt854xr.ws-eu114.gitpod.io',
 '.herokuapp.com']
 
 
-# Application definition
+# Application definitio
 
 INSTALLED_APPS = [
     'django.contrib.admin',
